37f40edd232f3809fd790304dcf948437dafb4c6
Prathamesh <prathameshaj2004@gmail.com>
2024-08-05 23:36:10 +0530

    Changes

---
+++ JAVASCRIPT/003-varletconst/index.js
@@ -0,0 +1,15 @@
+
+// var a = 45;
+// var a = "p"
+let b = "Prathamesh";
+const author = "Prathamesh"
+author = 5 // Throws an error because constant cannot be changed
+b = 4
+const Prathamesh = 0;
+let c = null
+let d  = undefined  
+{
+  let b = 'this'
+  console.log(b)
+}
+console.log(b)
